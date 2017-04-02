41c30d8014b794fde0df295224e6db8f8b775c82
AlexYeates <AlexYeates@hotmail.com>
2017-04-02 17:28:49 +0100

    Testing whether I can just call the animation function within a while loop.

--- js/app.js
+++ js/app.js
@@ -10,56 +10,58 @@ $(() => {
   // trying to get the mouseover remove an element from the array if i can't get the callback in the animation to only fire if it reaches the top...
   pop.mouseover(function() {
     $(this).removeAttr('id');
-  //  missedBalloons.pop();
+    //  missedBalloons.pop();
   });
 
   //These will generate the balloons so long as missedBalloons is less than 3.
-  animation();
+  while (missedBalloons < 3) {
+    animation();
+  }
 
   //Maybe add the hour glass animation so the balloons will sway as they try to reach the top?
   function animation(){
-    while (missedBalloons < 3) {
-      slowBalloons.setAttribute('id', 'slow');
-      const slow = {
-        duration: 1000,
-        easing: 'linear'
-      };
-      $('.board')
-      .find(slowBalloons)
-      .animate({
-        bottom: 300
-      }, slow);
-      $.extend(true, {}, slow, {
-        complete: missedBalloons.push(slowBalloons)
-      });
-      mediumBalloons.setAttribute('id', 'medium');
-      const medium = {
-        duration: 800,
-        easing: 'linear'
-      };
-      $('.board')
-      .find(mediumBalloons)
-      .animate({
-        bottom: 400
-      }, medium);
-      $.extend(true, {}, medium, {
-        complete: missedBalloons.push(mediumBalloons)
-      });
-      fastBalloons.setAttribute('id', 'fast');
-      const fast = {
-        duration: 600,
-        easing: 'linear'
-      };
-      $('.board')
-      .find(fastBalloons)
-      .animate({
-        bottom: 500
-      }, fast);
-      $.extend(true, {}, fast, {
-        complete: missedBalloons.push(fastBalloons)
-      });
-    }
+    // while (missedBalloons < 3) {
+    slowBalloons.setAttribute('id', 'slow');
+    const slow = {
+      duration: 1000,
+      easing: 'linear'
+    };
+    $('.board')
+    .find(slowBalloons)
+    .animate({
+      bottom: 300
+    }, slow);
+    $.extend(true, {}, slow, {
+      complete: missedBalloons.push(slowBalloons)
+    });
+    mediumBalloons.setAttribute('id', 'medium');
+    const medium = {
+      duration: 800,
+      easing: 'linear'
+    };
+    $('.board')
+    .find(mediumBalloons)
+    .animate({
+      bottom: 400
+    }, medium);
+    $.extend(true, {}, medium, {
+      complete: missedBalloons.push(mediumBalloons)
+    });
+    fastBalloons.setAttribute('id', 'fast');
+    const fast = {
+      duration: 600,
+      easing: 'linear'
+    };
+    $('.board')
+    .find(fastBalloons)
+    .animate({
+      bottom: 500
+    }, fast);
+    $.extend(true, {}, fast, {
+      complete: missedBalloons.push(fastBalloons)
+    });
   }
+  //}
   console.log(missedBalloons);
 
   //A timer for 30 seconds will end the game
