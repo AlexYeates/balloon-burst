9c8a1a8854f0ae742904fe12107c6608ac088736
AlexYeates <AlexYeates@hotmail.com>
2017-04-02 17:04:56 +0100

    Spelling.

--- js/app.js
+++ js/app.js
@@ -1,69 +1,59 @@
 $(() => {
 
   let missedBalloons = [];
-  let slowBallons = $('.slow-area')[Math.floor(Math.random()*$('.slow-area').length)];
-  let mediumBallons = $('.medium-area')[Math.floor(Math.random()*$('.medium-area').length)];
-  let fastBallons = $('.fast-area')[Math.floor(Math.random()*$('.fast-area').length)];
+  let slowBalloons = $('.slow-area')[Math.floor(Math.random()*$('.slow-area').length)];
+  let mediumBalloons = $('.medium-area')[Math.floor(Math.random()*$('.medium-area').length)];
+  let fastBalloons = $('.fast-area')[Math.floor(Math.random()*$('.fast-area').length)];
   const pop = $('li');
-
-  // generateBalloons();
-  slowAnimation();
 
   //A mouseover event will take the balloon's ID away, therefore making it pop
   pop.mouseover(function() {
     $(this).removeAttr('id');
   });
 
-  //An if statment will check whether missedBalloons has three ballons. If it doesn't balloons will be generated and be animated to go to the top of the board.
+  //These will generate the balloons so long as missedBalloons is less than 3.
+  mediumAnimation();
+  slowAnimation();
 
-  //Once the animations work and +1 to missedBalloons, add a loop to this function to check if missedBalloons is at 3, and if not to keep generating balloons while the countdown lasts
-  // function generateBalloons() {
-  //   // if (missedBalloons < 3) {
-  //     //while (missedBalloons < 3) {
-  //     //for (var i = 0; i < missedBalloons[3]; i++) {
-  //     slowBallons.setAttribute('id', 'slow');
-  //     mediumBallons.setAttribute('id', 'medium');
-  //     fastBallons.setAttribute('id', 'fast');
-  //   }
-
-  // generateBalloons();
 
   //Maybe add the hour glass animation so the balloons will sway as they try to reach the top?
   function slowAnimation(){
     while (missedBalloons < 3) {
-      slowBallons.setAttribute('id', 'slow');
+      slowBalloons.setAttribute('id', 'slow');
       var options = {
         duration: 1000,
         easing: 'linear'
       };
       $('.board')
-      .find(slowBallons)
+      .find(slowBalloons)
       .animate({
         bottom: 300
       }, options);
       $.extend(true, {}, options, {
-        complete: missedBalloons.push(slowBallons)
+        complete: missedBalloons.push(slowBalloons)
+      });
+    }
+  }
+
+  function mediumAnimation() {
+    while (missedBalloons < 3) {
+      mediumBalloons.setAttribute('id', 'medium');
+      var options = {
+        duration: 800,
+        easing: 'linear'
+      };
+      $('.board')
+      .find(mediumBalloons)
+      .animate({
+        bottom: 400
+      }, options);
+      $.extend(true, {}, options, {
+        complete: missedBalloons.push(mediumBalloons)
       });
     }
   }
 
 
-  // function mediumAnimation() {
-  //   var options = {
-  //     duration: 800,
-  //     easing: 'linear'
-  //   };
-  //   $('.board')
-  //   .find(mediumBallons)
-  //   .animate({
-  //     bottom: 400
-  //   }, options);
-  //   $.extend(true, {}, options, {
-  //     complete: missedBalloons.push(mediumBallons)
-  //   });
-  // }
-  // mediumAnimation();
-  //
   // function fastAnimation() {
   //   var options = {
   //     duration: 600,
@@ -87,3 +77,33 @@ $(() => {
   //A timer for 30 seconds will end the game
 
 });
+
+
+//Older verision before merging generateBalloons with the animations:
+
+//An if statment will check whether missedBalloons has three ballons. If it doesn't balloons will be generated and be animated to go to the top of the board.
+
+// function generateBalloons() {
+//   // if (missedBalloons < 3) {
+//     //while (missedBalloons < 3) {
+//     //for (var i = 0; i < missedBalloons[3]; i++) {
+//     slowBallons.setAttribute('id', 'slow');
+//     mediumBallons.setAttribute('id', 'medium');
+//     fastBallons.setAttribute('id', 'fast');
+//   }
+
+// function mediumAnimation() {
+//   var options = {
+//     duration: 800,
+//     easing: 'linear'
+//   };
+//   $('.board')
+//   .find(mediumBallons)
+//   .animate({
+//     bottom: 400
+//   }, options);
+//   $.extend(true, {}, options, {
+//     complete: missedBalloons.push(mediumBallons)
+//   });
+// }
+// mediumAnimation();
