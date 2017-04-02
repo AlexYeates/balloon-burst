9b0d9624fd1d2a4d654e2a88605c15c1a389014e
AlexYeates <AlexYeates@hotmail.com>
2017-04-02 17:24:22 +0100

    Trying to make the mouseover event remove an element from the missedBalloon array if I can't make the callback in the animation work properly.

--- js/app.js
+++ js/app.js
@@ -7,19 +7,20 @@ $(() => {
   const pop = $('li');
 
   //A mouseover event will take the balloon's ID away, therefore making it pop
+  // trying to get the mouseover remove an element from the array if i can't get the callback in the animation to only fire if it reaches the top...
   pop.mouseover(function() {
     $(this).removeAttr('id');
+  //  missedBalloons.pop();
   });
 
   //These will generate the balloons so long as missedBalloons is less than 3.
   animation();
 
-
   //Maybe add the hour glass animation so the balloons will sway as they try to reach the top?
   function animation(){
     while (missedBalloons < 3) {
       slowBalloons.setAttribute('id', 'slow');
-      var options = {
+      const slow = {
         duration: 1000,
         easing: 'linear'
       };
@@ -27,67 +28,46 @@ $(() => {
       .find(slowBalloons)
       .animate({
         bottom: 300
-      }, options);
-      $.extend(true, {}, options, {
+      }, slow);
+      $.extend(true, {}, slow, {
         complete: missedBalloons.push(slowBalloons)
       });
       mediumBalloons.setAttribute('id', 'medium');
+      const medium = {
+        duration: 800,
+        easing: 'linear'
+      };
       $('.board')
       .find(mediumBalloons)
       .animate({
         bottom: 400
-      }, options);
-      $.extend(true, {}, options, {
+      }, medium);
+      $.extend(true, {}, medium, {
         complete: missedBalloons.push(mediumBalloons)
+      });
+      fastBalloons.setAttribute('id', 'fast');
+      const fast = {
+        duration: 600,
+        easing: 'linear'
+      };
+      $('.board')
+      .find(fastBalloons)
+      .animate({
+        bottom: 500
+      }, fast);
+      $.extend(true, {}, fast, {
+        complete: missedBalloons.push(fastBalloons)
       });
     }
   }
-  //
-  // function mediumAnimation() {
-  //   while (missedBalloons < 3) {
-  //     mediumBalloons.setAttribute('id', 'medium');
-  //     var options = {
-  //       duration: 800,
-  //       easing: 'linear'
-  //     };
-  //     $('.board')
-  //     .find(mediumBalloons)
-  //     .animate({
-  //       bottom: 400
-  //     }, options);
-  //     $.extend(true, {}, options, {
-  //       complete: missedBalloons.push(mediumBalloons)
-  //     });
-  //   }
-  // }
-
-
-  // function fastAnimation() {
-  //   var options = {
-  //     duration: 600,
-  //     easing: 'linear'
-  //   };
-  //   $('.board')
-  //   .find(fastBallons)
-  //   .animate({
-  //     bottom: 500
-  //   }, options);
-  //   $.extend(true, {}, options, {
-  //     complete: missedBalloons.push(fastBallons)
-  //   });
-  // }
-  // fastAnimation();
-
   console.log(missedBalloons);
-
-
 
   //A timer for 30 seconds will end the game
 
 });
 
 
-//Older verision before merging generateBalloons with the animations:
+//Older verision before merging generateBalloons with the animations, and merging the animations into one function:
 
 //An if statment will check whether missedBalloons has three ballons. If it doesn't balloons will be generated and be animated to go to the top of the board.
 
@@ -100,18 +80,18 @@ $(() => {
 //     fastBallons.setAttribute('id', 'fast');
 //   }
 
-// function mediumAnimation() {
+// function slowAnimation() {
 //   var options = {
-//     duration: 800,
+//     duration: 1000,
 //     easing: 'linear'
 //   };
 //   $('.board')
-//   .find(mediumBallons)
+//   .find(slowBallons)
 //   .animate({
-//     bottom: 400
+//     bottom: 300
 //   }, options);
 //   $.extend(true, {}, options, {
-//     complete: missedBalloons.push(mediumBallons)
+//     complete: missedBalloons.push(slowBallons)
 //   });
 // }
-// mediumAnimation();
+// slowAnimation();
