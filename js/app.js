c5497fd084cd1c5fde382f9a935e38f046f28026
AlexYeates <AlexYeates@hotmail.com>
2017-04-02 16:49:09 +0100

    Playing with while loops and moving the call functions around.

--- js/app.js
+++ js/app.js
@@ -6,68 +6,76 @@ $(() => {
   let fastBallons = $('.fast-area')[Math.floor(Math.random()*$('.fast-area').length)];
   const pop = $('li');
 
+  generateBalloons();
+  slowAnimation();
+
+
   //An if statment will check whether missedBalloons has three ballons. If it doesn't balloons will be generated and be animated to go to the top of the board.
 
   //Once the animations work and +1 to missedBalloons, add a loop to this function to check if missedBalloons is at 3, and if not to keep generating balloons while the countdown lasts
   function generateBalloons() {
     if (missedBalloons < 3) {
-    //for (var i = 0; i < missedBalloons[3]; i++) {
+      //while (missedBalloons < 3) {
+      //for (var i = 0; i < missedBalloons[3]; i++) {
       slowBallons.setAttribute('id', 'slow');
       mediumBallons.setAttribute('id', 'medium');
       fastBallons.setAttribute('id', 'fast');
     }
   }
-  generateBalloons();
-
+  // generateBalloons();
 
   //Maybe add the hour glass animation so the balloons will sway as they try to reach the top?
-  function slowAnimation() {
-    var options = {
-      duration: 1000,
-      easing: 'linear'
-    };
-    $('.board')
-    .find(slowBallons)
-    .animate({
-      bottom: 300
-    }, options);
-    $.extend(true, {}, options, {
-      complete: missedBalloons.push(slowBallons)
-    });
+  function slowAnimation(){
+    while (missedBalloons < 3) {
+      // function slowAnimation() {
+      var options = {
+        duration: 1000,
+        easing: 'linear'
+      };
+      $('.board')
+      .find(slowBallons)
+      .animate({
+        bottom: 300
+      }, options);
+      $.extend(true, {}, options, {
+        complete: missedBalloons.push(slowBallons)
+      });
+      // slowAnimation();
+    }
   }
-  slowAnimation();
 
-  function mediumAnimation() {
-    var options = {
-      duration: 800,
-      easing: 'linear'
-    };
-    $('.board')
-    .find(mediumBallons)
-    .animate({
-      bottom: 400
-    }, options);
-    $.extend(true, {}, options, {
-      complete: missedBalloons.push(mediumBallons)
-    });
-  }
-  mediumAnimation();
 
-  function fastAnimation() {
-    var options = {
-      duration: 600,
-      easing: 'linear'
-    };
-    $('.board')
-    .find(fastBallons)
-    .animate({
-      bottom: 500
-    }, options);
-    $.extend(true, {}, options, {
-      complete: missedBalloons.push(fastBallons)
-    });
-  }
-  fastAnimation();
+  // function mediumAnimation() {
+  //   var options = {
+  //     duration: 800,
+  //     easing: 'linear'
+  //   };
+  //   $('.board')
+  //   .find(mediumBallons)
+  //   .animate({
+  //     bottom: 400
+  //   }, options);
+  //   $.extend(true, {}, options, {
+  //     complete: missedBalloons.push(mediumBallons)
+  //   });
+  // }
+  // mediumAnimation();
+  //
+  // function fastAnimation() {
+  //   var options = {
+  //     duration: 600,
+  //     easing: 'linear'
+  //   };
+  //   $('.board')
+  //   .find(fastBallons)
+  //   .animate({
+  //     bottom: 500
+  //   }, options);
+  //   $.extend(true, {}, options, {
+  //     complete: missedBalloons.push(fastBallons)
+  //   });
+  // }
+  // fastAnimation();
 
   console.log(missedBalloons);
 
