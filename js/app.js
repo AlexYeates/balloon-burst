6ca09912b08dec97c2cf9b4f72fc1605c391a5d6
AlexYeates <AlexYeates@hotmail.com>
2017-04-03 14:33:21 +0100

    Added game over.

--- js/app.js
+++ js/app.js
@@ -1,28 +1,56 @@
-const balloonWidth  = 50;
-const balloonHeight = 50;
-let score           = 0;
+// const balloonWidth  = 50;
 
 $(() => {
-  setInterval(createBalloon, 500);
-  $(document).on('mouseover', '.balloon', function() {
-    $(this).remove();
-    score++;
-    console.log(score);
-  });
+
+  var Game = Game || {};
+
+  Game.balloonHeight = 50;
+  Game.score         = 1;
+  Game.gameOver      = false;
+
+  Game.startGame = function() {
+    Game.interval = setInterval(Game.createBalloon, 500);
+    $(document).on('mouseover', '.balloon', function() {
+      $(this).remove();
+      $(this).addClass('clicked');
+      $('#score').text(`Score: ${Game.score++}`);
+    });
+  };
+
+
+  Game.createBalloon = function createBalloon() {
+    console.log(Game.balloon);
+    Game.board    = $('.board');
+    Game.balloon  = $('<div class="balloon"></div>');
+    Game.balloon.css('right', Game.randomStartingPosition());
+    Game.board.append(Game.balloon);
+    Game.balloon.animate({
+      top: `-${Game.balloonHeight}px`
+    }, {
+      duration: 1500,
+      step: Game.gameOverCheck,
+      complete: function() {
+        // clearInterval(Game.interval);
+        if (!($(this).hasClass('clicked'))) {
+          console.log('gameover');
+          Game.gameOver = true;
+        }
+      }
+    });
+  };
+
+  Game.gameOverCheck = function() {
+    if (Game.gameOver === true) {
+      console.log('GAAAAAME OVER YO');
+      $(this).stop();
+      $(this).remove();
+    }
+  };
+
+  Game.randomStartingPosition = function randomStartingPosition() {
+    return Math.floor(Math.random() * 1000);
+  };
+
+  Game.startGame();
+
 });
-
-function createBalloon() {
-  const $body    = $('body');
-  const $balloon = $('<div class="balloon"></div>');
-  $balloon.css('right', randomStartingPosition());
-  $body.append($balloon);
-  $balloon.animate({
-    top: `-${balloonHeight}px`
-  }, 1500, function() {
-    console.log('hit');
-  });
-}
-
-function randomStartingPosition() {
-  return Math.floor(Math.random() * $(document).width() - balloonWidth);
-}
